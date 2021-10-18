e69e3d6602232f5bcb76f4b0650a9e867f999283
hirontan <hirontan@sora.flights>
2021-10-18 23:24:50 +0900

    [Update] 試しにHello Worldと打ち込んでみる

--- hello_world/App.js
+++ hello_world/App.js
@@ -70,6 +70,9 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="Add">
+            Hello World
+          </Section>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
